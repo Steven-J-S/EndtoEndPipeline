0456afd93030bfc85691841ffaa58a380876bff2
StevenLaptop <steven_supit@hotmail.com>
2019-11-06 11:31:20 +0100

    Initial commit, repo made, .gitignore updated

---
+++ main_script.py
@@ -0,0 +1,27 @@
+''''
+This module contains the main script to run the compound interest calculators
+
+TODO: make custom exception class? i.e. CalculatorError(Exception)
+'''
+
+from compound_interest_calculator import compound_profit
+
+def main():
+    '''Main script function to run the compound interest calculators'''
+    try:
+        init_value = float(input('Input initial value:'))
+        interest = float(input('Input interest:'))
+        try:
+            cprofit = compound_profit(init_value, interest)
+            print('Profit with ${} initial value, and {} interest after 1 period: $'.format(init_value, interest), cprofit)
+        except:
+            print('Something went wrong in the calculation!') # TODO: use explicit exception
+    except ValueError as e:
+        print('Input values should be integers or floats!\n Args: ', e.args)
+    finally:
+        print('Script ended!')
+
+
+if __name__ == "__main__":
+    # Run the scripts
+    main()
